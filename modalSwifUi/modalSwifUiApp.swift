59bd0dab2e7b24d2b2d1d6735d7457ffe566bd17
sebasMuller <muller.sevas@gmail.com>
2021-08-27 14:58:09 -0300

    Initial Commit

---
+++ modalSwifUi/modalSwifUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  modalSwifUiApp.swift
+//  modalSwifUi
+//
+//  Created by Sebastian Muller on 27/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct modalSwifUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
