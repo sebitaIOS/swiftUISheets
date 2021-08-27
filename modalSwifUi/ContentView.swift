b7b455d5bd06688674f43b29a80fb27fcb0ac417
sebasMuller <muller.sevas@gmail.com>
2021-08-27 19:36:03 -0300

    improvments

--- modalSwifUi/ContentView.swift
+++ modalSwifUi/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showModal: Bool = false
+    @State private var selectedFlag : String = ""
+    let flags = ["🏴󠁧󠁢󠁳󠁣󠁴󠁿","🇮🇴","🇶🇦","🇹🇩","🇧🇹"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        List {
+            
+            ForEach(flags, id: \.self) { flag in
+                
+                HStack {
+                    Text(flag).font(.custom("Arial", size: 100))
+                }.onTapGesture {
+                    self.selectedFlag = flag
+                    self.showModal.toggle()
+                }
+                
+            }
+            
+            
+        }.sheet(isPresented: $showModal){
+            Text(selectedFlag)
+                .font(.custom("Arial", size: 200))
+        }
     }
 }
 
